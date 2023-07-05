ac43d8670c4ecc790d9d5ea04775db5783dc8ff8
Simon Walker <s.r.walker101@googlemail.com>
2023-07-05 17:18:38 +0100

    Set up gio example

--- shell.nix
+++ shell.nix
@@ -1,7 +1,14 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs;
+let
+  apple-frameworks = with darwin.apple_sdk.frameworks; [
+    Metal
+    QuartzCore
+    AppKit
+  ];
+in
 mkShell {
   buildInputs = [
     go
-  ];
+  ] ++ lib.optionals stdenv.isDarwin apple-frameworks;
 }
