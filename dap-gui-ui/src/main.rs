857c3cbb370f35d69ae990f6a1068a391474d3e0
Simon Walker <s.r.walker101@googlemail.com>
2023-07-15 23:03:16 +0100

    Include syntax highlighting

--- dap-gui-ui/src/main.rs
+++ dap-gui-ui/src/main.rs
@@ -1,12 +1,34 @@
 use anyhow::Result;
 use eframe::egui;
 
+mod syntax_highlighting;
+
 #[derive(Default)]
 struct MyApp {}
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |_ui| {});
+        egui::SidePanel::right("right_panel")
+            .resizable(false)
+            .show(ctx, |ui| {
+                ui.label("right panel");
+                // split into
+                // - variables
+                // - stack frames
+                // - breakpoints
+            });
+        egui::TopBottomPanel::bottom("bottom_panel").show(ctx, |ui| {
+            ui.label("bottom panel");
+            // split into
+            // - repl
+            // - output?
+        });
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("central panel");
+            // code
+            let example_code = include_str!("./main.rs");
+            syntax_highlighting::code_view_ui(ui, example_code);
+        });
     }
 }
 
