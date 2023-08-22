5f7a4eae9af312291d7fe946269ff4ce6a32ea18
Simon Walker <s.r.walker101@googlemail.com>
2023-08-22 21:31:49 +0100

    Fix compile error for keyboard events example

--- dap-gui-ui/examples/keyboard_events.rs
+++ dap-gui-ui/examples/keyboard_events.rs
@@ -2,7 +2,7 @@ use eframe::egui;
 use egui::*;
 
 fn main() -> Result<(), eframe::Error> {
-    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    tracing_subscriber::fmt::init();
     let options = eframe::NativeOptions::default();
     eframe::run_native(
         "Keyboard events",
