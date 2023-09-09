0d331e32583e72c86f56a9c031eb908f5d9e985b
Simon Walker <s.r.walker101@googlemail.com>
2023-09-09 21:43:50 +0100

    Include packages from main branch

--- dap-gui/src/main.rs
+++ dap-gui/src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::egui;
+
+#[cfg(feature = "sentry")]
+macro_rules! setup_sentry {
+    () => {
+        log::info!("setting up sentry for crash reporting");
+        let _guard = sentry::init((
+            "https://f08b65bc9944ecbb855f1ebb2cadcb92@o366030.ingest.sentry.io/4505663159926784",
+            sentry::ClientOptions {
+                release: sentry::release_name!(),
+                ..Default::default()
+            },
+        ));
+    };
+}
+
+#[cfg(not(feature = "sentry"))]
+macro_rules! setup_sentry {
+    () => {};
+}
+
+#[derive(Default)]
+struct MyApp {}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("Hello world");
+        });
+    }
+}
+
+fn main() -> Result<(), eframe::Error> {
+    setup_sentry!();
+
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(1024.0, 768.0)),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "DAP GUI",
+        options,
+        Box::new(move |_| Box::new(MyApp::default())),
+    )
 }
