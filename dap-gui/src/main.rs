e0323c694b3d5e71274876d39d21d7a3cdc367e9
Simon Walker <s.r.walker101@googlemail.com>
2023-09-10 21:41:18 +0100

    Set up event handling in ui

--- dap-gui/src/main.rs
+++ dap-gui/src/main.rs
@@ -1,3 +1,13 @@
+use std::{
+    net::TcpStream,
+    sync::{
+        mpsc::{self, Receiver},
+        Arc, Mutex,
+    },
+    thread,
+};
+
+use dap_gui_client::events;
 use eframe::egui;
 
 #[cfg(feature = "sentry")]
@@ -19,8 +29,36 @@ macro_rules! setup_sentry {
     () => {};
 }
 
-#[derive(Default)]
-struct MyApp {}
+struct AppState;
+
+#[allow(dead_code)]
+struct MyApp {
+    client: dap_gui_client::Client,
+    app_state: Arc<Mutex<AppState>>,
+}
+
+impl MyApp {
+    pub fn new(client: dap_gui_client::Client, client_events: Receiver<events::Event>) -> Self {
+        // set up background thread watching events
+
+        let state = Arc::new(Mutex::new(AppState {}));
+        let background_state = Arc::clone(&state);
+        let this = Self {
+            client,
+            app_state: state,
+        };
+        thread::spawn(move || {
+            for event in client_events {
+                handle_event(event, Arc::clone(&background_state));
+            }
+        });
+        this
+    }
+}
+
+fn handle_event(_event: events::Event, state_m: Arc<Mutex<AppState>>) {
+    let _state = state_m.lock().unwrap();
+}
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
@@ -37,10 +75,13 @@ fn main() -> Result<(), eframe::Error> {
         initial_window_size: Some(egui::vec2(1024.0, 768.0)),
         ..Default::default()
     };
+    let (tx, rx) = mpsc::channel();
+    let stream = TcpStream::connect("127.0.0.1:5678").unwrap();
+    let client = dap_gui_client::Client::new(stream, tx).unwrap();
 
     eframe::run_native(
         "DAP GUI",
         options,
-        Box::new(move |_| Box::new(MyApp::default())),
+        Box::new(move |_| Box::new(MyApp::new(client, rx))),
     )
 }
