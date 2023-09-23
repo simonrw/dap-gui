81db3da7d3231e4ecbfd0925a91dba2b764ba0ca
Simon Walker <s.r.walker101@googlemail.com>
2023-09-23 21:22:56 +0100

    Add linux dependencies

--- shell.nix
+++ shell.nix
@@ -28,6 +28,14 @@ mkShell {
     custom-python
   ] ++ lib.optionals stdenv.isDarwin apple-deps ++ lib.optionals stdenv.isLinux [
     gdb
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXi
+    xorg.libXinerama
+    xorg.libXrandr
+    xorg.libXxf86vm
+    pkg-config
+    libglvnd
   ];
 
   LD_LIBRARY_PATH =
