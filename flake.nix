6cb88b2d2f09b6f68ce371e6d142e9d09bd71700
Simon Walker <s.r.walker101@googlemail.com>
2023-09-26 21:38:44 +0100

    Use rust overlay for consistent rust versions

--- flake.nix
+++ flake.nix
@@ -2,13 +2,17 @@
   description = "Flake utils demo";
 
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [
+          (import rust-overlay)
         ];
 
         pkgs = import nixpkgs {
@@ -17,7 +21,55 @@
       in
       {
         devShells = rec {
-          default = import ./shell.nix { inherit pkgs; };
+          default =
+            with pkgs;
+            let
+              apple-frameworks = with darwin.apple_sdk.frameworks; [
+                OpenGL
+                CoreServices
+                AppKit
+              ];
+              apple-libs = [
+                libiconv
+              ];
+
+              apple-deps = apple-frameworks ++ apple-libs;
+
+              custom-python = python3.withPackages (ps: with ps; [
+                debugpy
+                black
+                scapy
+                structlog
+              ]);
+
+            in
+            mkShell {
+              buildInputs = [
+                rust-bin.beta.latest.default
+                rust-bin.beta.latest.rust-analyzer
+                cargo-nextest
+                custom-python
+              ] ++ lib.optionals stdenv.isDarwin apple-deps ++ lib.optionals stdenv.isLinux [
+                gdb
+              ];
+
+              RUST_BACKTRACE = "1";
+
+              RUST_SRC_PATH = "${rust-bin.beta.latest.rust-src}";
+
+              RUST_LOG = "dap_gui=debug,end_to_end=debug,dap_gui_ui=debug,dap_gui_client=debug";
+
+              LD_LIBRARY_PATH =
+                if stdenv.isLinux then
+                  lib.makeLibraryPath [
+                    xorg.libX11
+                    xorg.libXcursor
+                    xorg.libXrandr
+                    xorg.libXi
+                    libglvnd
+                    vulkan-loader # TODO: needed?
+                  ] else "";
+            };
         };
       }
     );
