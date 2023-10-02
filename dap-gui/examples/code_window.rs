cb69a0ca8c9b61fd0825489940ac2aa34cc31b2f
Simon Walker <s.r.walker101@googlemail.com>
2023-10-02 20:39:51 +0100

    Update code window widget

--- dap-gui/examples/code_window.rs
+++ dap-gui/examples/code_window.rs
@@ -7,27 +7,41 @@ use eframe::{
 struct MyEguiApp {
     content: String,
     current_line: usize,
+    highlight_line: bool,
     breakpoint_positions: Vec<usize>,
 }
 
-struct BreakpointEditor<'a> {
+/// Code view that shows debugger related things
+struct CodeView<'a> {
+    /// Read-only view into the text content
     content: &'a str,
+    /// Optionally highlight the line the debugger has stopped on
     current_line: usize,
+    highlight_line: bool,
+    /// Line numbers to add breakpoint markers to
     breakpoint_positions: &'a [usize],
 }
 
-impl<'a> BreakpointEditor<'a> {
-    pub fn new(content: &'a str, current_line: usize, breakpoint_positions: &'a [usize]) -> Self {
+impl<'a> CodeView<'a> {
+    /// Create a new code view
+    pub fn new(
+        content: &'a str,
+        current_line: usize,
+        highlight_line: bool,
+        breakpoint_positions: &'a [usize],
+    ) -> Self {
         Self {
             content,
             current_line,
+            highlight_line,
             breakpoint_positions,
         }
     }
 }
 
-impl<'a> egui::Widget for BreakpointEditor<'a> {
+impl<'a> egui::Widget for CodeView<'a> {
     fn ui(mut self, ui: &mut egui::Ui) -> egui::Response {
+        // closure that defines the layout drop
         let mut layouter = |ui: &egui::Ui, s: &str, _wrap_width: f32| {
             let mut layout_job = LayoutJob::default();
             let indent = 16.0;
@@ -43,7 +57,7 @@ impl<'a> egui::Widget for BreakpointEditor<'a> {
                         },
                     );
                 };
-                if self.current_line == i {
+                if self.highlight_line && self.current_line == i {
                     // highlighted line
                     layout_job.append(
                         line,
@@ -74,7 +88,8 @@ impl MyEguiApp {
         let content = include_str!("../../test.py");
         Self {
             content: content.to_string(),
-            current_line: 0,
+            current_line: 10,
+            highlight_line: true,
             breakpoint_positions: vec![3, 10],
         }
     }
@@ -83,15 +98,17 @@ impl MyEguiApp {
 impl eframe::App for MyEguiApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
+            ui.checkbox(&mut self.highlight_line, "Highlight?");
             ui.add(
                 Slider::new(&mut self.current_line, 0..=self.content.lines().count() - 1)
                     .text("Highlight line")
                     .integer(),
             );
             egui::ScrollArea::vertical().show(ui, |ui| {
-                ui.add(BreakpointEditor::new(
+                ui.add(CodeView::new(
                     &self.content,
                     self.current_line,
+                    self.highlight_line,
                     &self.breakpoint_positions,
                 ));
             });
