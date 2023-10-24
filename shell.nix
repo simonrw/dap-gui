9934aee3743cf00813103348d8e5a6376a5f8673
Simon Walker <s.r.walker101@googlemail.com>
2023-10-24 21:56:48 +0100

    Swap flake for shell.nix
    This way we don't have to keep the packages updated.

---
+++ shell.nix
@@ -0,0 +1,52 @@
+{ pkgs ? import <nixpkgs> { } }:
+with pkgs;
+let
+  apple-frameworks = with darwin.apple_sdk.frameworks; [
+    OpenGL
+    CoreServices
+    AppKit
+  ];
+  apple-libs = [
+    libiconv
+  ];
+
+  apple-deps = apple-frameworks ++ apple-libs;
+
+  custom-python = python3.withPackages (ps: with ps; [
+    debugpy
+    black
+    scapy
+    structlog
+  ]);
+in
+mkShell {
+  buildInputs = [
+    cargo
+    rustc
+    cargo-nextest
+    cargo-flamegraph
+    custom-python
+    cargo-hack
+    act
+  ] ++ lib.optionals stdenv.isDarwin apple-deps ++ lib.optionals stdenv.isLinux [
+    gdb
+    simplescreenrecorder
+    cargo-llvm-cov
+  ];
+  RUST_BACKTRACE = "1";
+
+  RUST_SRC_PATH = "${rustPlatform.rustLibSrc}";
+
+  RUST_LOG = "gui=trace,end_to_end=debug,transport=debug,dap_gui_client=debug";
+
+  LD_LIBRARY_PATH =
+    if stdenv.isLinux then
+      lib.makeLibraryPath [
+        xorg.libX11
+        xorg.libXcursor
+        xorg.libXrandr
+        xorg.libXi
+        libglvnd
+        vulkan-loader # TODO: needed?
+      ] else "";
+}
