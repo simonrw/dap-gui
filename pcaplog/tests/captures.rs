5a98c4021f7224ee0856a29ad6ff529865dd80f2
Simon Walker <s.r.walker101@googlemail.com>
2023-12-10 18:45:12 +0000

    Use rstest to make test cases

--- pcaplog/tests/captures.rs
+++ pcaplog/tests/captures.rs
@@ -1,7 +1,8 @@
-use std::{collections::HashMap, io::IsTerminal, path::PathBuf};
+use std::{io::IsTerminal, path::PathBuf};
 
 use anyhow::Context;
 use pcaplog::extract_messages;
+use rstest::rstest;
 use tracing_subscriber::EnvFilter;
 
 #[derive(Debug)]
@@ -11,53 +12,30 @@ pub struct Failure {
     pub got: usize,
 }
 
-#[test]
-fn captures() -> anyhow::Result<()> {
+#[rstest]
+#[trace]
+#[case("../captures/vscode/vscode-attach-connect.pcapng", 34)]
+#[trace]
+#[case("../captures/vscode/dlv-debug-session.pcapng", 80)]
+#[trace]
+#[case("../captures/vscode/full-session-multiple-breakpoints.pcapng", 108)]
+#[trace]
+#[case("../captures/vscode/full-session-testpy.pcapng", 93)]
+#[trace]
+#[case("../captures/vscode/session1.pcapng", 74)]
+#[trace]
+#[case("../captures/vscode/session2.pcapng", 169)]
+#[trace]
+#[case("../captures/vscode/stepover-go.pcapng", 16)]
+#[trace]
+#[case("../captures/vscode/vscode-attach-connect.pcapng", 34)]
+fn capture(#[case] path: &str, #[case] expected_count: usize) -> anyhow::Result<()> {
     init_test_logger();
-    let expected_counts = {
-        let mut counts = HashMap::new();
-        // TODO: update these counts
-        // counts.insert(PathBuf::from("../captures/pycharm/startup.pcapng"), 0);
-        // counts.insert(
-        //     PathBuf::from("../captures/vscode/dlv-debug-session.pcapng"),
-        //     0,
-        // );
-        // counts.insert(
-        //     PathBuf::from("../captures/vscode/full-session-multiple-breakpoints.pcapng"),
-        //     0,
-        // );
-        // counts.insert(
-        //     PathBuf::from("../captures/vscode/full-session-testpy.pcapng"),
-        //     0,
-        // );
-        // counts.insert(PathBuf::from("../captures/vscode/session2.pcapng"), 0);
-        // counts.insert(PathBuf::from("../captures/vscode/stepover-go.pcapng"), 0);
-        counts.insert(
-            PathBuf::from("../captures/vscode/vscode-attach-connect.pcapng"),
-            34,
-        );
-        counts
-    };
 
-    let files = glob::glob("../captures/**/*.pcap*").context("listing capture files")?;
-    let mut failures = Vec::new();
-    for file in files {
-        let file = file.context("invalid file")?;
-        if let Some(count) = expected_counts.get(&file) {
-            let messages = extract_messages(&file).context("extracting messages")?;
-            if messages.len() != *count {
-                failures.push(Failure {
-                    file: file.clone(),
-                    expected: *count,
-                    got: messages.len(),
-                });
-            }
-        } else {
-            tracing::warn!("file {} missing expected value", file.display());
-            // TODO: raise error if capture not found
-        }
-    }
-    assert_eq!(failures.len(), 0, "Failures: {:?}", failures);
+    let messages = extract_messages(path).context("extracting messages")?;
+
+    assert_eq!(messages.len(), expected_count);
+
     Ok(())
 }
 
