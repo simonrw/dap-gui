82a98085008385b5a2ab711c6b9f7b3aca48aeb3
Simon Walker <s.r.walker101@googlemail.com>
2023-12-14 12:13:23 +0000

    Use flake instead of shell.nix
    This way the packages I use to develop are consistent

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [(import rust-overlay)];
+      };
+    in {
+      formatter = pkgs.alejandra;
+
+      devShells.default = import ./shell.nix {inherit pkgs;};
+    });
+}
--- shell.nix
+++ shell.nix
@@ -1,61 +1,51 @@
-let
-  overlays = [
-    (import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"))
-  ];
-in
-
-{ pkgs ? import <nixpkgs> { inherit overlays; } }:
-with pkgs;
-
-let
+{pkgs, ...}:
+with pkgs; let
   apple-frameworks = with darwin.apple_sdk.frameworks; [
     OpenGL
     CoreServices
     AppKit
   ];
-  apple-libs = [
-    libiconv
-  ];
+  apple-libs = [libiconv];
 
   apple-deps = apple-frameworks ++ apple-libs;
 
-  custom-python = python3.withPackages (ps: with ps; [
-    debugpy
-    black
-    scapy
-    structlog
-  ]);
+  custom-python =
+    python3.withPackages (ps: with ps; [debugpy black scapy structlog]);
 in
-mkShell {
-  buildInputs = [
-    rust-bin.beta.latest.default
-    rust-analyzer
-    cargo-nextest
-    cargo-flamegraph
-    custom-python
-    cargo-hack
-    act
-    go
-    delve
-  ] ++ lib.optionals stdenv.isDarwin apple-deps ++ lib.optionals stdenv.isLinux [
-    gdb
-    simplescreenrecorder
-    cargo-llvm-cov
-  ];
-  RUST_BACKTRACE = "1";
+  mkShell {
+    buildInputs =
+      [
+        rust-bin.beta.latest.default
+        rust-analyzer
+        cargo-nextest
+        cargo-flamegraph
+        custom-python
+        cargo-hack
+        act
+        go
+        delve
+      ]
+      ++ lib.optionals stdenv.isDarwin apple-deps
+      ++ lib.optionals stdenv.isLinux [gdb simplescreenrecorder cargo-llvm-cov];
 
-  RUST_SRC_PATH = "${rustPlatform.rustLibSrc}";
+    env = {
+      RUST_BACKTRACE = "1";
 
-  RUST_LOG = "gui=trace,end_to_end=debug,transport=debug,dap_gui_client=debug,debugger=debug";
+      RUST_SRC_PATH = "${rustPlatform.rustLibSrc}";
 
-  LD_LIBRARY_PATH =
-    if stdenv.isLinux then
-      lib.makeLibraryPath [
-        xorg.libX11
-        xorg.libXcursor
-        xorg.libXrandr
-        xorg.libXi
-        libglvnd
-        vulkan-loader # TODO: needed?
-      ] else "";
-}
+      RUST_LOG = "gui=trace,end_to_end=debug,transport=debug,dap_gui_client=debug,debugger=debug";
+
+      LD_LIBRARY_PATH =
+        if stdenv.isLinux
+        then
+          lib.makeLibraryPath [
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXrandr
+            xorg.libXi
+            libglvnd
+            vulkan-loader # TODO: needed?
+          ]
+        else "";
+    };
+  }
