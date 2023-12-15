acced60f3385c0446b9971eb234488d3561284fb
Simon Walker <s.r.walker101@googlemail.com>
2023-12-15 23:00:48 +0000

    Add hand-writen parser benchmark

--- transport/benches/parser.rs
+++ transport/benches/parser.rs
@@ -3,14 +3,23 @@ use std::io::{BufReader, Cursor};
 
 use transport::Reader;
 
-pub fn parser_benchmark(c: &mut Criterion) {
+pub fn nom_parser_benchmark(c: &mut Criterion) {
     let message =
         Cursor::new("Content-Length: 37\r\n\r\n{\"type\":\"event\",\"event\":\"terminated\"}\n");
 
     let mut reader = transport::NomReader::new(BufReader::new(message));
 
-    c.bench_function("poll message", |b| b.iter(|| reader.poll_message()));
+    c.bench_function("nom parser", |b| b.iter(|| reader.poll_message()));
 }
 
-criterion_group!(benches, parser_benchmark);
+pub fn hand_written_parser_benchmark(c: &mut Criterion) {
+    let message =
+        Cursor::new("Content-Length: 37\r\n\r\n{\"type\":\"event\",\"event\":\"terminated\"}\n");
+
+    let mut reader = transport::HandWrittenReader::new(BufReader::new(message));
+
+    c.bench_function("hand written parser", |b| b.iter(|| reader.poll_message()));
+}
+
+criterion_group!(benches, nom_parser_benchmark, hand_written_parser_benchmark);
 criterion_main!(benches);
