41c6a90d92a9087ed67ed2e31e06d6d60d62ce64
Simon Walker <s.r.walker101@googlemail.com>
2023-12-17 22:49:40 +0000

    Add tests for message combinations

--- transport/src/reader/hand_written_reader.rs
+++ transport/src/reader/hand_written_reader.rs
@@ -91,7 +91,8 @@ mod tests {
     use super::HandWrittenReader;
 
     macro_rules! execute_test {
-        ($bodies:expr, $match_expr:pat) => {{
+        // multiple bodies for single message
+        ($($body:expr),+ => $match_expr:pat) => {{
             let port = get_random_tcp_port().expect("getting random port");
             let server =
                 TcpListener::bind(format!("127.0.0.1:{port}")).expect("binding to address");
@@ -101,10 +102,7 @@ mod tests {
 
             let mut reader = HandWrittenReader::new(BufReader::new(conn));
 
-            // send message
-            for body in $bodies {
-                write!(&mut client, "{body}").expect("sending message");
-            }
+            $(write!(&mut client, "{}", $body).expect("sending message");)+
 
             let message = reader.poll_message().expect("polling message");
 
@@ -115,14 +113,61 @@ mod tests {
                 None => anyhow::bail!("no message found"),
             }
         }};
+
+        // multiple messages for single body
+        ($body:expr => $($match_expr:pat),+) => {{
+            let port = get_random_tcp_port().expect("getting random port");
+            let server =
+                TcpListener::bind(format!("127.0.0.1:{port}")).expect("binding to address");
+            let mut client =
+                TcpStream::connect(format!("127.0.0.1:{port}")).expect("connecting to server");
+            let (conn, _) = server.accept().expect("accepting connection");
+
+            let mut reader = HandWrittenReader::new(BufReader::new(conn));
+
+
+
+            write!(&mut client, "{}", $body).expect("sending message");
+
+            $(
+
+            let message = reader.poll_message().expect("polling message");
+
+            match message {
+                Some(msg) => {
+                    assert!(matches!(msg, $match_expr));
+                }
+                None => anyhow::bail!("no message found"),
+            }
+
+            )+
+        }};
     }
 
     #[test]
     fn single_message() -> anyhow::Result<()> {
-        let bodies =
-            vec!["Content-Length: 37\r\n\r\n{\"type\":\"event\",\"event\":\"terminated\"}"];
+        let body = "Content-Length: 37\r\n\r\n{\"type\":\"event\",\"event\":\"terminated\"}";
 
-        execute_test!(bodies, Message::Event(events::Event::Terminated));
+        execute_test!(body => Message::Event(events::Event::Terminated));
+
+        Ok(())
+    }
+
+    #[test]
+    fn split_between_requests() -> anyhow::Result<()> {
+        execute_test!(
+            "Content-Length: 37\r\n\r\n{\"ty",
+            "pe\":\"event\",\"event\":\"terminated\"}" => 
+        Message::Event(events::Event::Terminated));
+
+        Ok(())
+    }
+
+    #[test]
+    fn multiple_messages() -> anyhow::Result<()> {
+        let body = "Content-Length: 37\r\n\r\n{\"type\":\"event\",\"event\":\"terminated\"}Content-Length: 37\r\n\r\n{\"type\":\"event\",\"event\":\"terminated\"}";
+
+        execute_test!(body => Message::Event(events::Event::Terminated), Message::Event(events::Event::Terminated));
 
         Ok(())
     }
