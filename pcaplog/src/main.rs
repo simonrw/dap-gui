ad3c3d23da22b8a87982f85ee5d6e3d6b84e64d8
Simon Walker <s.r.walker101@googlemail.com>
2023-12-27 14:06:27 +0000

    Output pcaplog logging to stderr

--- pcaplog/src/main.rs
+++ pcaplog/src/main.rs
@@ -20,6 +20,7 @@ struct Messages(Vec<transport::Message>);
 fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::from_default_env())
+        .with_writer(std::io::stderr)
         .init();
 
     let args = Args::parse();
