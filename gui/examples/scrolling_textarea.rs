07c49f5d5100cc9338aeed1421509a0559336dcb
Simon Walker <s.r.walker101@googlemail.com>
2024-01-21 22:26:17 +0000

    Add debug printing when cursor changes

--- gui/examples/scrolling_textarea.rs
+++ gui/examples/scrolling_textarea.rs
@@ -1,12 +1,65 @@
-use eframe::egui::{self, Visuals};
+use std::f32;
+
+use eframe::{
+    egui::{self, TextEdit, Visuals},
+    epaint::text::cursor::RCursor,
+};
+
+enum Position {
+    Top,
+    Mid,
+    Bottom,
+}
 
 #[derive(Default)]
-struct App {}
+struct App {
+    contents: String,
+    last_cursor: Option<RCursor>,
+}
+
+impl App {
+    fn scroll_to(&mut self, _position: Position) {}
+}
 
 impl eframe::App for App {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::SidePanel::left("left-panel").show(ctx, |ui| {
+            if ui.button("scroll to top").clicked() {
+                self.scroll_to(Position::Top);
+            }
+
+            if ui.button("scroll to midpoint").clicked() {
+                self.scroll_to(Position::Mid);
+            }
+
+            if ui.button("scroll to bottom").clicked() {
+                self.scroll_to(Position::Bottom);
+            }
+        });
+
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label("Hello world");
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                let output = TextEdit::multiline(&mut self.contents)
+                    .desired_width(f32::INFINITY)
+                    .code_editor()
+                    .show(ui);
+
+                // tracing::trace!(cursor = ?output.cursor_range.map(|r| r.primary), "cursor position");
+
+                if let Some(new_cursor) = output.cursor_range.map(|r| r.primary.rcursor) {
+                    match self.last_cursor.as_mut() {
+                        Some(last_cursor) => {
+                            if new_cursor != *last_cursor {
+                                *last_cursor = new_cursor;
+                                tracing::debug!(cursor = ?*last_cursor, "new cursor position");
+                            }
+                        }
+                        None => self.last_cursor = Some(new_cursor),
+                    }
+                }
+
+                output.response
+            });
         });
     }
 }
@@ -33,7 +86,10 @@ fn main() {
                 ..Default::default()
             };
             cc.egui_ctx.set_style(style);
-            let app = App::default();
+            let app = App {
+                contents: include_str!("./scrolling_textarea.rs").to_string(),
+                ..Default::default()
+            };
             Box::new(app)
         }),
     )
