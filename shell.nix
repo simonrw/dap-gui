34ae684868868fa9119c7ca1f8e1b3c11a7a0645
Simon Walker <s.r.walker101@googlemail.com>
2024-03-31 22:22:43 +0100

    Configure new system for wayland
    Or not really... using xwayland specifically here

--- shell.nix
+++ shell.nix
@@ -14,29 +14,40 @@ with pkgs; let
 
   toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
 in
-mkShell {
-  buildInputs =
-    [
-      toolchain
-      rust-analyzer-unwrapped
-      cargo-flamegraph
-      custom-python
-      cargo-hack
-      cargo-expand
-      act
-      go
-      delve
-      clang
-      hyperfine
-    ]
-    ++ lib.optionals stdenv.isDarwin apple-deps
-    ++ lib.optionals stdenv.isLinux [ mold gdb simplescreenrecorder cargo-llvm-cov ];
+  mkShell rec {
+    packages =
+      [
+        toolchain
+        rust-analyzer-unwrapped
+        cargo-flamegraph
+        custom-python
+        cargo-hack
+        act
+        go
+        delve
+        clang
+        hyperfine
+      ]
+      ++ lib.optionals stdenv.isDarwin apple-deps
+      ++ lib.optionals stdenv.isLinux [
+        cargo-llvm-cov
+        gdb
+        libglvnd
+        libxkbcommon
+        mold
+        simplescreenrecorder
+        vulkan-loader # TODO: needed?
+        wayland
+        xorg.libX11
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXrandr
+      ];
 
     shellHook = ''
       export RUST_BUILD_BASE="$HOME/.cache/rust-builds"
       WORKSPACE_ROOT=$(cargo metadata --no-deps --offline 2>/dev/null | jq -r ".workspace_root")
       PACKAGE_BASENAME=$(basename $WORKSPACE_ROOT)
-
       # Run cargo with target set to $RUST_BUILD_BASE/$PACKAGE_BASENAME
       export CARGO_TARGET_DIR="$RUST_BUILD_BASE/$PACKAGE_BASENAME"
     '';
@@ -46,18 +57,12 @@ mkShell {
       RUST_LOG = "gui=debug,end_to_end=debug,transport=debug,dap_gui_client=debug,debugger=debug";
       RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
 
+      # TMP: don't run iced gui under wayland
+      WAYLAND_DISPLAY = "";
+
       LD_LIBRARY_PATH =
         if stdenv.isLinux
-        then
-          lib.makeLibraryPath [
-            libxkbcommon
-            xorg.libX11
-            xorg.libXcursor
-            xorg.libXrandr
-            xorg.libXi
-            libglvnd
-            vulkan-loader # TODO: needed?
-          ]
+        then lib.makeLibraryPath packages
         else "";
     };
-}
+  }
