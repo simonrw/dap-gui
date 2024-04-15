651d62af34ba55ca5b952ae0afcd781fea827468
Simon Walker <s.r.walker101@googlemail.com>
2024-04-15 22:33:05 +0100

    Create new transport package

--- transport/src/lib.rs
+++ transport/src/lib.rs
@@ -1,21 +1,14 @@
-//! DAP Client
-//!
-//! This crate contains code to create a DAP client.
-pub mod bindings;
-mod client;
-pub mod events;
-#[cfg(nom)]
-mod parse;
-pub mod reader;
-mod request_store;
-pub mod requests;
-pub mod responses;
-pub mod types;
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
 
-pub use client::Client;
-pub use client::Message;
-pub use client::Received;
-pub use reader::Reader;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-/// The default port the DAP protocol listens on
-pub const DEFAULT_DAP_PORT: u16 = 5678;
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
