1e1f90c10d2a701af6e0b4ddbf46f6a04bac82bf
Simon Walker <s.r.walker101@googlemail.com>
2024-04-20 13:39:39 +0100

    Revert "Add blocking command example"
    This reverts commit 4b33e05376553296ba2d55177a84221cb9299992.

--- gui2/src/main.rs
+++ gui2/src/main.rs
@@ -1,12 +1,10 @@
-use iced::widget::{button, column, container, row, text, Container};
+use iced::widget::{column, container, row, text, Container};
 use iced::{executor, Application, Color, Command, Element, Length, Settings};
 use iced_aw::Tabs;
 
 #[derive(Debug, Clone)]
 enum Message {
     TabSelected(TabId),
-    Foo,
-    GotResult(usize),
 }
 
 fn title<'a, Message>(input: impl ToString) -> Container<'a, Message> {
@@ -92,15 +90,6 @@ impl Application for DebuggerApp {
         match self {
             Self::Paused { active_tab } => match message {
                 Message::TabSelected(selected) => *active_tab = selected,
-                Message::Foo => {
-                    return Command::perform(
-                        async { tokio::task::spawn_blocking(move || 10).await.unwrap() },
-                        Message::GotResult,
-                    )
-                }
-                Message::GotResult(value) => {
-                    println!("Got result: {value}");
-                }
             },
             _ => {}
         }
@@ -112,7 +101,6 @@ impl Application for DebuggerApp {
     }
 
     fn view(&self) -> iced::Element<'_, Self::Message> {
-        return column![button("increment").on_press(Message::Foo),].into();
         let sidebar = column![self.view_call_stack(), self.view_breakpoints(),]
             .height(Length::Fill)
             .width(Length::Fill);
