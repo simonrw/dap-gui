4fbd3bdccf552e5f27332607ebb45c237e6e932a
Simon Walker <s.r.walker101@googlemail.com>
2024-05-18 22:39:36 +0100

    Iced GUI
    Implement an initial version of a GUI using iced rather than egui.
    
    For now we keep this in a separate crate until I choose the "main"
    implementation to use.

---
+++ gui2/src/lib.rs
@@ -0,0 +1,197 @@
+use std::collections::HashSet;
+
+use clap::Parser;
+use code_view::{CodeViewer, CodeViewerAction};
+use dark_light::Mode;
+use iced::widget::{column, container, row, text, text_editor, Container};
+use iced::{executor, Application, Color, Command, Element, Length};
+use iced_aw::Tabs;
+
+pub mod code_view;
+mod highlight;
+
+#[derive(Debug, Parser)]
+pub struct Args {
+    /// debug rendering
+    #[clap(short, long)]
+    debug: bool,
+}
+
+#[derive(Debug, Clone)]
+pub enum Message {
+    TabSelected(TabId),
+    CodeViewer(CodeViewerAction),
+}
+
+fn title<'a, Message>(input: impl ToString) -> Container<'a, Message> {
+    container(text(input).size(30)).padding(20)
+}
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub enum TabId {
+    Variables,
+    Repl,
+}
+
+#[derive(Debug)]
+pub enum DebuggerApp {
+    #[allow(dead_code)]
+    Initialising,
+    #[allow(dead_code)]
+    Running { breakpoints: HashSet<usize> },
+    Paused {
+        args: Args,
+        active_tab: TabId,
+        content: text_editor::Content,
+        breakpoints: HashSet<usize>,
+        scrollable_id: iced::widget::scrollable::Id,
+    },
+    #[allow(dead_code)]
+    Terminated,
+}
+
+impl DebuggerApp {
+    // view helper methods
+    fn view_call_stack(&self) -> iced::Element<'_, Message> {
+        title("Call Stack").width(Length::Fill).into()
+    }
+
+    fn view_breakpoints(&self) -> iced::Element<'_, Message> {
+        title("Breakpoints").width(Length::Fill).into()
+    }
+
+    fn view_main_content(&self) -> iced::Element<'_, Message> {
+        match self {
+            DebuggerApp::Initialising => todo!(),
+            DebuggerApp::Running { .. } => todo!(),
+            DebuggerApp::Paused {
+                ref content,
+                breakpoints,
+                scrollable_id,
+                ..
+            } => CodeViewer::new(
+                content,
+                breakpoints,
+                scrollable_id.clone(),
+                Message::CodeViewer,
+            )
+            .into(),
+            DebuggerApp::Terminated => todo!(),
+        }
+    }
+
+    fn view_variables_content(&self) -> iced::Element<'_, Message> {
+        text("variables").into()
+    }
+
+    fn view_repl_content(&self) -> iced::Element<'_, Message> {
+        text("repl").into()
+    }
+
+    fn view_bottom_panel(&self) -> iced::Element<'_, Message> {
+        if let Self::Paused { active_tab, .. } = self {
+            Tabs::new(Message::TabSelected)
+                .tab_icon_position(iced_aw::tabs::Position::Top)
+                .push(
+                    TabId::Variables,
+                    iced_aw::TabLabel::Text("Variables".to_string()),
+                    self.view_variables_content(),
+                )
+                .push(
+                    TabId::Repl,
+                    iced_aw::TabLabel::Text("Repl".to_string()),
+                    self.view_repl_content(),
+                )
+                .set_active_tab(active_tab)
+                .into()
+        } else {
+            panic!("programming error: state {self:?} should not have a bottom panel");
+        }
+    }
+}
+
+impl Application for DebuggerApp {
+    type Executor = executor::Default;
+    type Theme = iced::Theme;
+    type Flags = ();
+    type Message = Message;
+
+    fn new(_flags: Self::Flags) -> (Self, Command<Self::Message>) {
+        let args = Args::parse();
+
+        let this = Self::Paused {
+            args,
+            active_tab: TabId::Variables,
+            content: text_editor::Content::with_text(include_str!("main.rs")),
+            breakpoints: HashSet::new(),
+            scrollable_id: iced::widget::scrollable::Id::unique(),
+        };
+        (this, Command::none())
+    }
+
+    fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
+        #[allow(clippy::single_match)]
+        match self {
+            Self::Paused {
+                active_tab,
+                breakpoints,
+                content,
+                scrollable_id,
+                ..
+            } => match message {
+                Message::TabSelected(selected) => *active_tab = selected,
+                Message::CodeViewer(CodeViewerAction::BreakpointChanged(bp)) => {
+                    if breakpoints.contains(&bp) {
+                        breakpoints.remove(&bp);
+                    } else {
+                        breakpoints.insert(bp);
+                    }
+                }
+                Message::CodeViewer(CodeViewerAction::EditorAction(action)) => {
+                    tracing::debug!(?action, "got editor action");
+                    content.perform(action)
+                }
+                Message::CodeViewer(CodeViewerAction::ScrollCommand { offset, .. }) => {
+                    return iced::widget::scrollable::scroll_to(scrollable_id.clone(), offset);
+                }
+            },
+            _ => {}
+        }
+        Command::none()
+    }
+
+    fn title(&self) -> String {
+        "DebuggerApp".to_string()
+    }
+
+    fn view(&self) -> iced::Element<'_, Self::Message> {
+        match self {
+            DebuggerApp::Paused { args, .. } => {
+                let sidebar = column![self.view_call_stack(), self.view_breakpoints(),]
+                    .height(Length::Fill)
+                    .width(Length::Fill);
+
+                let main_content = column![self.view_main_content(), self.view_bottom_panel(),]
+                    .height(Length::Fill);
+
+                let mut result = Element::from(row![
+                    sidebar.width(Length::Fixed(300.0)),
+                    main_content.width(Length::Fill),
+                ]);
+
+                if args.debug {
+                    result = result.explain(Color::from_rgb(1.0, 0.0, 0.0));
+                }
+                result
+            }
+            _ => todo!(),
+        }
+    }
+
+    fn theme(&self) -> Self::Theme {
+        match dark_light::detect() {
+            Mode::Dark | Mode::Default => iced::Theme::Dark,
+            Mode::Light => iced::Theme::Light,
+        }
+    }
+}
