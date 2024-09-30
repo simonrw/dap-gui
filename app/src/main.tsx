fb6753cfe5e9984ca46ea86e457ef5c23b0935ee
Simon Walker <s.r.walker101@googlemail.com>
2024-09-30 23:38:26 +0100

    Add basic react prototype

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,9 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { StrictMode } from 'react'
+import { ThemeProvider } from "@/components/theme-provider"
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ThemeProvider defaultTheme="system">
+      <App />
+    </ThemeProvider>
+  </StrictMode>,
+)
