c4be02805bcf24bf2e1182830e4a047c808894e2
Simon Walker <s.r.walker101@googlemail.com>
2024-11-28 16:30:28 +0000

    Set up `run` function

--- tui/src/main.rs
+++ tui/src/main.rs
@@ -1,14 +1,29 @@
+use color_eyre::eyre::{self, Context};
 use crossterm::event::{self, Event};
-use ratatui::Frame;
-fn main() {
+use ratatui::{prelude::Backend, Frame, Terminal};
+
+fn main() -> eyre::Result<()> {
     let mut terminal = ratatui::init();
+    terminal.clear().wrap_err("clearing the terminal")?;
+    let app_result = run(terminal);
+    ratatui::restore();
+    app_result
+}
+
+fn run<T>(mut terminal: Terminal<T>) -> eyre::Result<()>
+where
+    T: Backend,
+{
     loop {
-        terminal.draw(draw).expect("failed to draw frame");
-        if matches!(event::read().expect("failed to read event"), Event::Key(_)) {
+        terminal.draw(draw).wrap_err("failed to draw frame")?;
+        if matches!(
+            event::read().wrap_err("failed to read event")?,
+            Event::Key(_)
+        ) {
             break;
         }
     }
-    ratatui::restore();
+    Ok(())
 }
 
 fn draw(_frame: &mut Frame) {}
