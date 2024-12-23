bfa3879c1a37d4ba0cc05ee55688792d0c7740b5
Simon Walker <s.r.walker101@googlemail.com>
2024-12-23 21:57:24 +0000

    Use stable channel

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
         url = "github:oxalica/rust-overlay";
