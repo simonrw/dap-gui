61cd69dc151f2bc20f86dc8fa00ebb9882093ca4
Simon Walker <s.r.walker101@googlemail.com>
2024-12-24 15:14:26 +0000

    Handle state in embedded ipython

--- pythondap/test.py
+++ pythondap/test.py
@@ -5,51 +5,38 @@ from IPython import start_ipython
 import subprocess as sp
 from IPython.terminal.embed import InteractiveShellEmbed
 import argparse
-# os.environ["RUST_LOG"] = "error"
 
 from pythondap import Debugger, PausedFrame
 
-class InteractiveShellEmbedEnhanced(InteractiveShellEmbed):
-    def __init__(self, breakpoints, file, *args, **kwargs):
-        banner1 = kwargs.pop("banner1", "")
-        exit_msg = kwargs.pop("exit_msg", "")
-
-        super().__init__(banner1=banner1, exit_msg=exit_msg, *args, **kwargs)
-
+class DebugSession:
+    def __init__(self, breakpoints, file):
         self.d = Debugger(breakpoints=breakpoints, file=file)
         self.stack: list = []
         self.frame: PausedFrame | None = None
 
-    def resume(self):
+        self.resume = self.fn_resume
+
+    def fn_resume(self):
         state = self.d.resume()
+        print(f"received state: {state=}")
+        if not state:
+            return
         self.stack = state.stack
         self.frame = state.paused_frame
         return state
 
 
+try:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b", "--breakpoint", type=int, nargs="*", default=[])
+    parser.add_argument("-f", "--file", required=False)
+    args = parser.parse_args()
 
+    # start debuee in background process
+    p = sp.Popen(["make", "run-attach"])
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-b", "--breakpoint", type=int, nargs="*", default=[])
-parser.add_argument("-f", "--file", required=False)
-args = parser.parse_args()
+    ns = DebugSession(args.breakpoint, args.file)
 
-
-# start debuee in background process
-p = sp.Popen(["make", "run-attach"])
-
-
-ipshell = InteractiveShellEmbedEnhanced(breakpoints=args.breakpoint, file=args.file)
-
-def resume():
-    return ipshell.resume()
-
-def stack():
-    return ipshell.stack
-
-def frame():
-    return ipshell.frame
-
-ipshell.run_line_magic("autocall", "2")
-setattr(ipshell.__class__, 'user_global_ns', property(lambda self: self.user_ns))
-ipshell()
+    start_ipython(user_ns=ns.__dict__)
+finally:
+    p.kill()
