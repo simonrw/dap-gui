06da828b4c2e12959598486148b14e7115adbb60
Simon Walker <s.r.walker101@googlemail.com>
2025-01-18 23:17:35 +0000

    Use ratatui

--- repl/src/main.rs
+++ repl/src/main.rs
@@ -1,54 +1,16 @@
-use std::{io::Write, path::PathBuf};
+use std::path::PathBuf;
 
 use clap::Parser;
-use color_eyre::eyre::{self, Context};
-use debugger::Breakpoint;
-use debugger::Debugger;
-
-struct App {
-    debugger: Debugger,
-    input_buffer: String,
-}
-
-impl App {
-    fn new(debugger: Debugger) -> Self {
-        Self {
-            debugger,
-            input_buffer: String::new(),
-        }
-    }
-
-    fn loop_step(&mut self) -> eyre::Result<ShouldQuit> {
-        tracing::trace!("locking stdout");
-        print!("> ");
-        tracing::trace!("prompt written, flushing stdout");
-        std::io::stdout().flush()?;
-        tracing::trace!("stdout flushed");
-
-        tracing::trace!("reading from stdin");
-        let n = std::io::stdin().read_line(&mut self.input_buffer)?;
-        tracing::trace!(%n, "read bytes from stdin");
-        let input = self.input_buffer.trim().to_owned();
-        tracing::trace!(%input, "parsed command");
-
-        let res = self.handle_input(&input).context("handling command");
-        self.input_buffer.clear();
-        res
-    }
-
-    fn handle_input(&mut self, input: &str) -> eyre::Result<ShouldQuit> {
-        match input {
-            "q" => return Ok(ShouldQuit::True),
-            "c" => {
-                tracing::debug!("executing continue command");
-                self.debugger.r#continue().context("resuming execution")?;
-            }
-            "" => return Ok(ShouldQuit::False),
-            other => println!("Unhandled commmand: '{}'", other),
-        }
-        Ok(ShouldQuit::False)
-    }
-}
+use color_eyre::{eyre::Context, Result};
+use crossterm::event::{self, Event, KeyCode};
+use debugger::{Breakpoint, Debugger};
+use ratatui::{
+    layout::{Constraint, Layout, Position},
+    style::{Color, Style},
+    text::{Line, Span},
+    widgets::{Block, List, ListItem, Paragraph},
+    DefaultTerminal, Frame,
+};
 
 #[derive(Debug, Parser)]
 struct Args {
@@ -60,9 +22,140 @@ struct Args {
     #[clap(short, long)]
     breakpoints: Vec<Breakpoint>,
 }
+struct App {
+    debugger: Debugger,
+    input: String,
+    character_index: usize,
+    messages: Vec<String>,
+}
 
-fn main() -> eyre::Result<()> {
-    color_eyre::install().context("installing color_eyre")?;
+impl App {
+    fn new(debugger: Debugger) -> Self {
+        Self {
+            debugger,
+            input: String::new(),
+            character_index: 0,
+            messages: Vec::new(),
+        }
+    }
+
+    fn move_cursor_right(&mut self) {
+        let cursor_moved_right = self.character_index.saturating_add(1);
+        self.character_index = self.clamp_cursor(cursor_moved_right);
+    }
+
+    fn enter_char(&mut self, new_char: char) {
+        let index = self.byte_index();
+        self.input.insert(index, new_char);
+        self.move_cursor_right();
+    }
+
+    fn byte_index(&self) -> usize {
+        self.input
+            .char_indices()
+            .map(|(i, _)| i)
+            .nth(self.character_index)
+            .unwrap_or(self.input.len())
+    }
+
+    fn clamp_cursor(&self, new_cursor_pos: usize) -> usize {
+        new_cursor_pos.clamp(0, self.input.chars().count())
+    }
+
+    fn run_command(&mut self) {
+        // TODO: execute debugger command
+        match self.input.as_str() {
+            "c" => {
+                tracing::debug!("executing continue command");
+                self.debugger.r#continue().expect("continuing execution");
+            }
+            other => tracing::warn!(%other, "unhandled command"),
+        }
+
+        self.input.clear();
+        self.reset_cursor();
+    }
+
+    fn delete_char(&mut self) {
+        let is_not_cursor_leftmost = self.character_index != 0;
+        if is_not_cursor_leftmost {
+            // Method "remove" is not used on the saved text for deleting the selected char.
+            // Reason: Using remove on String works on bytes instead of the chars.
+            // Using remove would require special care because of char boundaries.
+
+            let current_index = self.character_index;
+            let from_left_to_current_index = current_index - 1;
+
+            // Getting all characters before the selected character.
+            let before_char_to_delete = self.input.chars().take(from_left_to_current_index);
+            // Getting all characters after selected character.
+            let after_char_to_delete = self.input.chars().skip(current_index);
+
+            // Put all characters together except the selected one.
+            // By leaving the selected one out, it is forgotten and therefore deleted.
+            self.input = before_char_to_delete.chain(after_char_to_delete).collect();
+            self.move_cursor_left();
+        }
+    }
+
+    fn move_cursor_left(&mut self) {
+        let cursor_moved_left = self.character_index.saturating_sub(1);
+        self.character_index = self.clamp_cursor(cursor_moved_left);
+    }
+
+    fn reset_cursor(&mut self) {
+        self.character_index = 0;
+    }
+
+    fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
+        loop {
+            terminal.draw(|frame| self.draw(frame))?;
+            if let Event::Key(key) = event::read()? {
+                match key.code {
+                    KeyCode::Enter => self.run_command(),
+                    KeyCode::Char(to_insert) => self.enter_char(to_insert),
+                    KeyCode::Backspace => self.delete_char(),
+                    KeyCode::Left => self.move_cursor_left(),
+                    KeyCode::Right => self.move_cursor_right(),
+                    KeyCode::Esc => break Ok(()),
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        let vertical = Layout::vertical([Constraint::Length(3), Constraint::Min(1)]);
+        let [input_area, messages_area] = vertical.areas(frame.area());
+
+        let input = Paragraph::new(self.input.as_str())
+            .style(Style::default().fg(Color::Yellow))
+            .block(Block::bordered().title("Input"));
+        frame.render_widget(input, input_area);
+        frame.set_cursor_position(Position::new(
+            // Draw the cursor at the current position in the input field.
+            // This position is can be controlled via the left and right arrow key
+            input_area.x + self.character_index as u16 + 1,
+            // Move one line down, from the border to the input line
+            input_area.y + 1,
+        ));
+
+        let messages: Vec<ListItem> = self
+            .messages
+            .iter()
+            .enumerate()
+            .map(|(i, m)| {
+                let content = Line::from(Span::raw(format!("{i}: {m}")));
+                ListItem::new(content)
+            })
+            .collect();
+        let messages = List::new(messages).block(Block::bordered().title("Messages"));
+        frame.render_widget(messages, messages_area);
+    }
+}
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
     tracing_subscriber::fmt::init();
 
     let args = Args::parse();
@@ -76,20 +169,9 @@ fn main() -> eyre::Result<()> {
     }
     tracing::debug!("breakpoints added");
 
-    let mut app = App::new(debugger);
-    tracing::debug!("debugger set up");
-    loop {
-        match app.loop_step() {
-            Ok(ShouldQuit::True) => break,
-            Ok(ShouldQuit::False) => {}
-            Err(e) => eyre::bail!("Error running command: {e}"),
-        }
-    }
-
-    Ok(())
-}
-
-enum ShouldQuit {
-    True,
-    False,
+    let terminal = ratatui::init();
+    let app = App::new(debugger);
+    let result = app.run(terminal);
+    ratatui::restore();
+    result
 }
