ea419d473cfe17f905e68579191c0f95884ad0c4
Simon Walker <s.r.walker101@googlemail.com>
2025-01-25 22:10:53 +0000

    Remove custom rust cache dir
    Otherwise I don't take advantage of other rust project's built outputs

--- shell.nix
+++ shell.nix
@@ -48,14 +48,6 @@ in
       RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
     };
 
-    shellHook = ''
-      export RUST_BUILD_BASE="$HOME/.cache/rust-builds"
-      WORKSPACE_ROOT=$(cargo metadata --no-deps --offline 2>/dev/null | jq -r ".workspace_root")
-      PACKAGE_BASENAME=$(basename $WORKSPACE_ROOT)
-      # Run cargo with target set to $RUST_BUILD_BASE/$PACKAGE_BASENAME
-      export CARGO_TARGET_DIR="$RUST_BUILD_BASE/$PACKAGE_BASENAME"
-    '';
-
     postVenvCreation = ''
       python -m pip install \
         debugpy \
