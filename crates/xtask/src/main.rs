8212ca828359b475dbe761232fafb66142e30b7f
Simon Walker <s.r.walker101@googlemail.com>
2026-01-31 00:42:48 +0000

    Add xtask pattern

---
+++ crates/xtask/src/main.rs
@@ -0,0 +1,74 @@
+use std::{
+    env,
+    process::{Command, Stdio},
+};
+
+use clap::Parser;
+
+#[derive(Parser)]
+enum Args {
+    Test,
+    Doctest,
+    InitVenv,
+}
+
+macro_rules! run {
+    ($command:expr) => {{
+        if !Command::new($command)
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .output().unwrap().status.success() {
+                panic!("command failed");
+        }
+    }};
+
+    ($command:expr, $($arg:expr),+) => {{
+        if !Command::new($command)
+            .args([
+                $($arg),+
+            ])
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .output().unwrap().status.success() {
+                panic!("command failed");
+        }
+    }};
+}
+
+macro_rules! cargo {
+    ($($arg:expr),+) => {{
+        let cargo = env::var("CARGO").unwrap_or_else(|_| "cargo".to_string());
+        if !Command::new(cargo)
+            .args([
+                $($arg),+
+            ])
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .output().unwrap().status.success() {
+                panic!("command failed");
+        }
+    }};
+}
+
+fn main() {
+    let args = Args::parse();
+
+    match args {
+        Args::Test => {
+            cargo!(
+                "nextest",
+                "run",
+                "--locked",
+                "--all-features",
+                "--all-targets"
+            );
+        }
+        Args::Doctest => {
+            cargo!("test", "--locked", "--all-features", "--doc");
+        }
+        Args::InitVenv => {
+            run!("python", "-m", "venv", ".venv");
+            run!(".venv/bin/python", "-m", "pip", "install", "debugpy");
+        }
+    };
+}
